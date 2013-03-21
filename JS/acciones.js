5811bbc4bf5603bd60309d8c0f7c61fb8a845744
AL25J11 <zairaMartinez50@gmail.com>
2013-03-21 08:10:13 -0600

    1.2

--- JS/acciones.js
+++ JS/acciones.js
@@ -26,5 +26,19 @@ document.addEventListener("deviceready",function (){
 			navigator.notification. alert ("Deslizo a la derecha",function(){},"Practica1","Aseptar");
 			});
 	 });
+	 	$('izquierda').swipeleft(function(){
+navigator.notification.comfirm("que quiere hacer"),function(opcion)
+{
+switch(opcion)
+{
+case 1:
+navigator.notification.beep(1);
+break;
+case 2:
+navigator.notification.vibrate(500);
+break;
+}
+},"practica1","beep","vibrar",("canselar");
+});
 	
 },false);
